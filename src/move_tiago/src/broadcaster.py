8e2d62345af5d11a23899de7c80952dd37f3402f
Allister Gough (k21066336) <k21066336@nmslab-004126.nms.kcl.ac.uk>
2024-03-11 17:40:22 +0000

    get transformations working

--- src/move_tiago/src/broadcaster.py
+++ src/move_tiago/src/broadcaster.py
@@ -6,35 +6,36 @@ from geometry_msgs.msg import TransformStamped, Point, Pose
 from tiago_controllers import BaseController
 
 # broadcast transformation from point cloud to map
-def broadcast_transform(p: Point):
-    """
-    Broadcast a simple transform message from source_frame to target_frame.
-    """
-    tf_broadcaster = tf2_ros.TransformBroadcaster()
-
-    source_frame = "xtion_rgb_optical_frame"
-    target_frame = "map"
-
-    try:
-        while not rospy.is_shutdown():
-            # Create a TransformStamped message
-            transform_msg = TransformStamped()
-            transform_msg.header.stamp = rospy.Time.now()
-            transform_msg.header.frame_id = source_frame
-            transform_msg.child_frame_id = target_frame
-            transform_msg.transform.translation.x = p.x  # Example translation (1 meter along x-axis)
-            transform_msg.transform.translation.y = p.y  # Example translation (no movement along y-axis)
-            transform_msg.transform.translation.z = 0.0  # Example translation (no movement along z-axis)
-            transform_msg.transform.rotation.w = 1.0  # Example rotation (no rotation)
-
-            # Broadcast the transform message
-            tf_broadcaster.sendTransform(transform_msg)
-
-            rospy.loginfo(f'broadcasting transformation of {p.x}, {p.y}')
-
-            rospy.sleep(1.0)  # Sleep for 1 second
-    except rospy.ROSInterruptException:
-        rospy.loginfo("ROS node interrupted.")
+# here i am creating my own transform message. Use the one that already exists. This could be the problem.
+# def broadcast_transform(p: Point):
+#     """
+#     Broadcast a simple transform message from source_frame to target_frame.
+#     """
+#     tf_broadcaster = tf2_ros.TransformBroadcaster()
+#
+#     source_frame = "xtion_rgb_optical_frame"
+#     target_frame = "map"
+#
+#     try:
+#         while not rospy.is_shutdown():
+#             # Create a TransformStamped message
+#             transform_msg = TransformStamped()
+#             transform_msg.header.stamp = rospy.Time.now()
+#             transform_msg.header.frame_id = source_frame
+#             transform_msg.child_frame_id = target_frame
+#             transform_msg.transform.translation.x = p.x  # Example translation (1 meter along x-axis)
+#             transform_msg.transform.translation.y = p.y  # Example translation (no movement along y-axis)
+#             transform_msg.transform.translation.z = 0.0  # Example translation (no movement along z-axis)
+#             transform_msg.transform.rotation.w = 1.0  # Example rotation (no rotation)
+#
+#             # Broadcast the transform message
+#             tf_broadcaster.sendTransform(transform_msg)
+#
+#             rospy.loginfo(f'broadcasting transformation of {p.x}, {p.y}')
+#
+#             rospy.sleep(1.0)  # Sleep for 1 second
+#     except rospy.ROSInterruptException:
+#         rospy.loginfo("ROS node interrupted.")
 
 if __name__ == '__main__':
 
--- src/move_tiago/src/detect.py
+++ src/move_tiago/src/detect.py
@@ -5,13 +5,18 @@ import numpy as np
 from sensor_msgs.msg import Image, PointCloud2
 from sensor_msgs import point_cloud2
 from lasr_vision_msgs.srv import YoloDetection, YoloDetectionRequest
-from geometry_msgs.msg import Point, TransformStamped
+from geometry_msgs.msg import Point, PointStamped
+import tf2_geometry_msgs
 
 import tf2_ros
 import tf_conversions
 
+rospy.init_node('detect_coords_node')
+
 # create service proxy
 detect_service = rospy.ServiceProxy('/yolov8/detect', YoloDetection)
+tfb = tf2_ros.Buffer()
+tf_listener = tf2_ros.TransformListener(tfb)
 
 
 counter = 0
@@ -20,13 +25,16 @@ def callback(img: Image):
     global counter
     counter = counter + 1
 
+    x = 0
+    y = 0
+
     if counter % 20 == 0:
         # instantiate the service request
         request = YoloDetectionRequest()
         request.image_raw = img # sensor_msgs/Image
         request.dataset = "yolov8n-seg.pt" # YOLOv8 model, auto-downloads
         request.confidence = 0.5 # minimum confidence to include in results
-        request.nms = 0.3 # non maximal supression
+        request.nms = 0.3 # coord_publisher(point.x, point.y)n maximal supression
         # fill in service fields
 
         # send request
@@ -39,15 +47,19 @@ def callback(img: Image):
             rospy.loginfo("DETECTION")
             for resp in response.detected_objects:
 
-                point = calculate_average_point(resp.xyseg)
-
+                rospy.loginfo(f"Found {resp.name}")
+                point = calculate_average_point_and_transform(resp.xyseg) # calc where person is
+                x = point.point.x
+                y = point.point.y
                 print(point)
                 # coord_publisher(point.x, point.y)
                 # coord_publisher(1.45, 0.02) # hard coded test case
                 # publish average x and y to coordinates
-                coord_publisher(point.x, point.y)
+
                 # print(coord_publisher(point.x, point.y))
-                print(resp)
+
+                coord_publisher(x, y) # send point to movement controller
+
 
 # publish detected coordinates to 'coordinates' topic
 def coord_publisher(x, y):
@@ -60,29 +72,28 @@ def coord_publisher(x, y):
     # Set the rate at which to publish (in Hz)
     rate = rospy.Rate(1)  # 1 Hz
 
-    while not rospy.is_shutdown():
-        # Create a Point message
-        point_msg = Point()
+    # Create a Point message
+    point_msg = Point()
 
-        # Set the x and y coordinates
-        point_msg.x = x
-        point_msg.y = y
+    # Set the x and y coordinates
+    point_msg.x = x
+    point_msg.y = y
 
-        # Publish the Point message
-        pub.publish(point_msg)
+    # Publish the Point message
+    pub.publish(point_msg)
 
-        # Print the published coordinates
-        rospy.loginfo("Published coordinates: x = %.2f, y = %.2f", x, y)
+    # Print the published coordinates
+    rospy.loginfo("Published coordinates: x = %.2f, y = %.2f", x, y)
 
-        # Sleep to maintain the desired publishing rate
-        rate.sleep()
+    # Sleep to maintain the desired publishing rate
+    rate.sleep()
 
 # current x: 0.0007836426540656172
 # desired x:  0.5673575401306152
 
-def calculate_average_point(mask_seg):
+def calculate_average_point_and_transform(mask_seg):
     pcl = rospy.wait_for_message('/xtion/depth_registered/points', PointCloud2) # get point cloud
-    print(pcl.header) # frame of the point cloud
+    # print(pcl.header.frame_id) # frame of the point cloud
     depth_data = point_cloud2.read_points(pcl, field_names=("x", "y", "z"), skip_nans=True) # extract x, y, z from points
     depth_array = np.array(list(depth_data))
 
@@ -100,11 +111,22 @@ def calculate_average_point(mask_seg):
     average_y = (total_y / num_points) + 0.1
     average_z = total_z / num_points
 
-    return Point(x=average_x, y=average_y, z=average_z)
+    point = Point(x=average_x, y=average_y, z=average_z)
+    point_stamped = PointStamped()
+    point_stamped.point = point
+    point_stamped.header.frame_id = pcl.header.frame_id
+    point_stamped.header.stamp = rospy.Time(0)
 
+    try:
+        point_stamped = tfb.transform(point_stamped, "map")
+        rospy.loginfo("Point transformation successful")
+    except Exception:
+        rospy.loginfo("Transform unavailable")
+        pass
+
+    return point_stamped
 
 if __name__ == '__main__':
-    rospy.init_node('detect_coords_node')
     # make subscriber to camera topic + pass Image information + define callback function
     image_subscriber = rospy.Subscriber('/xtion/rgb/image_raw', Image, callback)
 
